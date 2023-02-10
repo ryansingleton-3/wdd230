a24b45f66c776fdfe9bfd02d59b36096d89b9dd7
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-02-10 12:42:05 -0500

    changed fetch call to https protocall

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -2,7 +2,7 @@ async function getWeather() {
   const apiKey = "8184f1e17133404b88c32358231002";
   const zipCode = "62960";
   const resp = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${zipCode}&aqi=no`
+    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${zipCode}&aqi=no`
   );
   const data = await resp.json();
   getWeatherSpecs(data);
