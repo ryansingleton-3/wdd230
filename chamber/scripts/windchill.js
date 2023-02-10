23052c49027ff4baf0a2ee5e1ca00d262732cb24
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-02-10 10:02:00 -0500

    zip code added

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -5,7 +5,7 @@ let windMPH = null;
 let windChill = null;
 
 const apiKey = "8184f1e17133404b88c32358231002";
-metropolisZipCode = "62960";
+const metropolisZipCode = "62960";
 async function getWeather() {
   resp = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=${apiKey}=${metropolisZipCode}&aqi=no`
