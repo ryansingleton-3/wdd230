2fa2d22705ce1826b99e3be1878b31125d3ef021
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-02-10 09:58:17 -0500

    formatted js

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -9,10 +9,9 @@ function toggleMenu() {
 const fullName = "Ryan Singleton";
 const currentYear = new Date().getFullYear();
 // const state = "Kentucky"
-let eventSpan = document.getElementById("event-span")
-let currentDay = new Date().getDay()
-let events = false
-
+let eventSpan = document.getElementById("event-span");
+let currentDay = new Date().getDay();
+let events = false;
 
 const onLoad = () => {
   let lastUpdated = `${new Date(document.lastModified)}`;
@@ -27,37 +26,33 @@ const onLoad = () => {
   let day = new Date().toDateString();
   let dayText = document.querySelector("#date-text");
   dayText.innerHTML = `${day}`;
-  currentDay == 1 || currentDay == 2 ? events = true : events = false
-    if (events == true) {
-        eventSpan.innerHTML = "🤝🏼 Chamber Meet and Greet - Wednesday at 7pm"
-        eventSpan.style.fontSize = "2.5rem"
-        eventSpan.style.textAlign = "center"
-        document.querySelector("body").style.display = "flex"
-        document.querySelector("body").style.flexWrap = "wrap"
-        document.querySelector("body").style.flexDirection = "column"
-        const bannerElement = document.createElement("div")
-        const announcement = document.createElement("p")
-        body = document.querySelector("body")
-        header = document.querySelector("header")
-        body.insertBefore(bannerElement, body.header)
-        bannerElement.appendChild(announcement)
-        announcement.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
-        bannerElement.style.border = "1px solid black"
-        bannerElement.style.margin = "0 auto"
-        bannerElement.style.width = "100%"
-        bannerElement.style.backgroundColor = "#002349"
-        announcement.style.color = "white"
-        announcement.style.textAlign = "center"
-        bannerElement.style.order = "-1"
-        bannerElement.style.marginBottom = "1%"
-
-        
-    } else {
-        eventSpan.innerHTML = "Become a member and receive email updates for upcoming events"
-        eventSpan.style.fontSize = "2rem"
-    }   
-  
+  currentDay == 1 || currentDay == 2 ? (events = true) : (events = false);
+  if (events == true) {
+    eventSpan.innerHTML = "🤝🏼 Chamber Meet and Greet - Wednesday at 7pm";
+    eventSpan.style.fontSize = "2.5rem";
+    eventSpan.style.textAlign = "center";
+    document.querySelector("body").style.display = "flex";
+    document.querySelector("body").style.flexWrap = "wrap";
+    document.querySelector("body").style.flexDirection = "column";
+    const bannerElement = document.createElement("div");
+    const announcement = document.createElement("p");
+    body = document.querySelector("body");
+    header = document.querySelector("header");
+    body.insertBefore(bannerElement, body.header);
+    bannerElement.appendChild(announcement);
+    announcement.innerHTML =
+      "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    bannerElement.style.border = "1px solid black";
+    bannerElement.style.margin = "0 auto";
+    bannerElement.style.width = "100%";
+    bannerElement.style.backgroundColor = "#002349";
+    announcement.style.color = "white";
+    announcement.style.textAlign = "center";
+    bannerElement.style.order = "-1";
+    bannerElement.style.marginBottom = "1%";
+  } else {
+    eventSpan.innerHTML =
+      "Become a member and receive email updates for upcoming events";
+    eventSpan.style.fontSize = "2rem";
+  }
 };
-
-
-
