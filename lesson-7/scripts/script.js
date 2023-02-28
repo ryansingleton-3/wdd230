ab461854f5179bfe70a0ef7befb7bc32e207fd0d
ryansingleton-3 <ryanbsingleton3@gmail.com>
2023-02-27 23:03:53 -0500

    finished lazy loading activity

---
+++ lesson-7/scripts/script.js
@@ -0,0 +1,33 @@
+const imagesToLoad = document.querySelectorAll("img[data-src]")
+console.log(imagesToLoad)
+
+const options = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+const loadImages = (img) => {
+    img.setAttribute("src", img.getAttribute("data-src"))
+    img.onload = () => {
+        img.removeAttribute("data-src")
+        img.className = "in"
+    };
+};
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, options)
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
